4e50b82b7cbe2f8ad245a2a9b52e686d8a470f61
Funa <194032449@6171ucoop.onmicrosoft.com>
2022-03-28 09:29:22 +0900

    プロジェクト ファイルを追加します。

---
+++ WinScreenSaverForYouTube/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace WinScreenSaverForYouTube
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.launchToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "開始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(486, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "停止";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(445, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "(min)";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "通知";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 200);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(131, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "再生する動画がありません";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(468, 23);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(486, 41);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "保存";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(12, 317);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(28, 15);
+            this.linkLabel1.TabIndex = 7;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "URL";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(379, 12);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(60, 23);
+            this.numericUpDown1.TabIndex = 8;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.launchToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(114, 48);
+            // 
+            // launchToolStripMenuItem
+            // 
+            this.launchToolStripMenuItem.Name = "launchToolStripMenuItem";
+            this.launchToolStripMenuItem.Size = new System.Drawing.Size(113, 22);
+            this.launchToolStripMenuItem.Text = "Launch";
+            this.launchToolStripMenuItem.Click += new System.EventHandler(this.launchToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(113, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 70);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(549, 23);
+            this.progressBar1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(573, 341);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "ScreenSaver";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private Button button3;
+        private LinkLabel linkLabel1;
+        private NumericUpDown numericUpDown1;
+        private NotifyIcon notifyIcon1;
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem launchToolStripMenuItem;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private ProgressBar progressBar1;
+    }
+}
---
+++ WinScreenSaverForYouTube/Form1.cs
@@ -0,0 +1,176 @@
+using System;
+using System.Windows.Forms;
+
+namespace WinScreenSaverForYouTube
+{
+    public partial class Form1 : Form
+    {
+        
+        static int sleepCounter = 0;
+        static bool isCountNumber = false;
+        string url = "https://www.youtube.com/";
+        private System.Windows.Forms.Timer viewTimer;
+        private System.Windows.Forms.Timer loopTimer;
+
+
+        // YouTubeを開く関数
+        public void ViewYouTube(string youtube_url)
+        {
+            var startInfo = new System.Diagnostics.ProcessStartInfo(youtube_url);
+            startInfo.UseShellExecute = true;
+            System.Diagnostics.Process.Start(startInfo);
+
+            viewTimer = new System.Windows.Forms.Timer();
+            viewTimer.Tick += new EventHandler(MyKeyEvent); // TimerEvent
+            viewTimer.Interval = 10000;  // 10秒後に実行
+            viewTimer.Start();
+        }
+
+        private void MyKeyEvent(object sender, EventArgs e)
+        {
+            SendKeys.Send("F");
+            viewTimer.Stop();
+        }
+
+
+
+        // Main関数
+        public Form1()
+        {
+            InitializeComponent();
+            //  拡大ボタンを無効化
+            this.MaximizeBox = false;
+            numericUpDown1.Value = 5;
+            label2.Text = "タイマーを設定して開始ボタンを押してください。\r\n";
+            label3.Text = "YouTubeのURLを設定してください。";
+
+            loopTimer = new System.Windows.Forms.Timer();
+            loopTimer.Tick += new EventHandler(MyLoopEvent); // TimerEvent
+            loopTimer.Interval = 10000; // 10秒毎に確認
+            loopTimer.Start();
+        }
+
+        private void MyLoopEvent(object sender, EventArgs e)
+        {
+            if (isCountNumber)
+            {
+                sleepCounter++;
+                if (numericUpDown1.Value * 60 - sleepCounter * 6 < 1)
+                {
+                    System.Media.SystemSounds.Asterisk.Play();  // 警告音
+                    ViewYouTube(url);
+                    isCountNumber = false;
+                    sleepCounter = 0;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label2.Text = "タイマーが開始されました。\r\n";
+            label2.Text += "設定時間：" + numericUpDown1.Value + "分\r\n";
+
+            isCountNumber = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label2.Text = "タイマーは停止中です。\r\n";
+            label2.Text += "タイマーを設定して開始ボタンを押してください。\r\n";
+            isCountNumber = false;
+            sleepCounter = 0;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // YouTubeのときのみTextを保存
+            if (textBox1.Text.Contains("https://www.youtube.com/watch?v="))
+            {
+                url = textBox1.Text;
+
+                label3.Text = url + "\r\nを保存しました\r\n";
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            if (url.Contains("https://www.youtube.com/"))
+            {
+                ViewYouTube(url);
+            }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            numericUpDown1.Minimum = 1;
+            numericUpDown1.Maximum = 30;
+        }
+
+        private void launchToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Visible = true;        //フォームの表示
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.WindowState = FormWindowState.Normal;
+            }
+            this.Activate();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //アイコンをトレイから取り除く
+            notifyIcon1.Visible = false;
+            //アプリケーションの終了
+            Application.Exit();
+        }
+
+        // 閉じるボタンでタスクトレイに最小化
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+            if (e.CloseReason != System.Windows.Forms.CloseReason.ApplicationExitCall)
+            {
+                e.Cancel = true;
+                this.Visible = false;
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+            this.Visible = true;
+            // 最小化を解除
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.WindowState = FormWindowState.Normal;
+            }
+            // 最前面に表示
+            this.Activate();
+        }
+    }
+}
